b5d7ccf1c3f595a6a5529a0565d87e9502610215
Rahul Roy <rahulroy.e1004@gmail.com>
2023-05-27 03:00:06 +0530

    Initial Commit

---
+++ Vajra Wallet/Vajra_WalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vajra_WalletApp.swift
+//  Vajra Wallet
+//
+//  Created by Rahul Roy on 27/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Vajra_WalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
